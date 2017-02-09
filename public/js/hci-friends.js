0e5004ecb6e6aa6918cbe1e19501c769a60f2652
Lam Pham <lamqpham101@gmail.com>
2017-02-09 11:31:04 -0800

    completed lab

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,6 +10,16 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	$("a.friend").click(friendClick);
+}
+
+function friendClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event   
+    var name = $(this).text();   
+    $(this).text(anagrammedName(name));
 }
 
 function anagrammedName(name) {
--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,11 @@
 var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
-	// Your code goes here
+
+	var name = req.query.name;
+    var description = req.query.description;
+    var imgURL = 'http://lorempixel.com/300/300/people';
+    data["friends"].push({name: name, description: description, imageURL: imgURL});
+	res.render('add'); 
+
  }
